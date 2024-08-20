0f9487864aad5c57c46ff9efab98b5925ebf639e
OriHaguel <orihaguel@gmail.com>
2024-08-20 13:07:14 +0300

    Add file changes

--- src/pages/GigIndex.jsx
+++
@@ -1,6 +0,0 @@
-import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-
-export function GigIndex() {
-
-}
--- src/pages/ReviewIndex.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-
-
-export function ReviewIndex() {
-}
