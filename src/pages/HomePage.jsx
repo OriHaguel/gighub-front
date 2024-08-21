515f7bf5f1dde80fe66b85b830436c4d41540538
alexl <alexlarik13@gmail.com>
2024-08-21 17:17:47 +0300

    Create demo advert area

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { AppHeader } from "../cmps/AppHeader"
 import { PopularServicesCarrousel } from "../cmps/PopularServicesCarrousel"
+import { AdvertArea } from "../cmps/AdvertArea"
 
 export function HomePage() {
 
@@ -13,12 +14,18 @@ export function HomePage() {
 
             <section>
                 <h1>Home sweet Home</h1>
-                <p>Welcome to our platform! Find everything you need, just like on Fiverr.</p>
+                <h1>GigHub</h1>
+                <p>Welcome to our platform GigHub! Find everything you need, just like on Fiverr.</p>
             </section>
 
             <section className="home-page-carrousel">
-                <p className="services-header">Popular Services</p>
+                <h1>Popular Services</h1>
                 <PopularServicesCarrousel />
+            </section>
+
+            <section className="advert-area">
+                <h1>Advert Area</h1>
+                <AdvertArea />
             </section>
 
         </div>
