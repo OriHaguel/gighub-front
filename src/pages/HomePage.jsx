e138a5f2d932af3b21a9ac685efc5eaa76fe5042
alexl <alexlarik13@gmail.com>
2024-08-21 14:50:57 +0300

    Create basic app header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
+import { AppHeader } from "../cmps/AppHeader"
+import React from "react"
+
 export function HomePage() {
+
+    console.log('hi')
     return (
-        <section>
-            <h1>Home sweet Home</h1>
-        </section >
+        <ul className="toys-list">
+            <AppHeader />
+            <section>
+                {/* <h1>Home sweet Home</h1> */}
+                <p>Welcome to our platform! Find everything you need, just like on Fiverr.</p>
+            </section>
+        </ul>
     )
 }
 
