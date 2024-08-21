5422d02b0ba12e3332d3a9135b93cf05915bc508
ForkliftOctober <ork16oct@gmail.com>
2024-08-21 17:26:36 +0300

    Add divs

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -5,226 +5,235 @@ export function AppFooter() {
 		<footer className='main-container flex space-between align-center'>
 			{/* Links */}
 			{/* Col 1 */}
-
 			<div className='footer-links'>
-				<ul>
-					<li>
-						<a href='/categories/graphics-design'>Graphics &amp; Design</a>
-					</li>
-					<li>
-						<a href='/categories/online-marketing'>Digital Marketing</a>
-					</li>
-					<li>
-						<a href='/categories/writing-translation'>Writing &amp; Translation</a>
-					</li>
-					<li>
-						<a href='/categories/video-animation'>Video &amp; Animation</a>
-					</li>
-					<li>
-						<a href='/categories/music-audio'>Music &amp; Audio</a>
-					</li>
-					<li>
-						<a href='/logo-maker?source=footer'>Fiverr Logo Maker</a>
-					</li>
-					<li>
-						<a href='/categories/programming-tech'>Programming &amp; Tech</a>
-					</li>
-					<li>
-						<a href='/categories/data'>Data</a>
-					</li>
-					<li>
-						<a href='/categories/business'>Business</a>
-					</li>
-					<li>
-						<a href='/categories/lifestyle'>Personal Growth &amp; Hobbies</a>
-					</li>
-					<li>
-						<a href='/categories/photography'>Photography</a>
-					</li>
-					<li>
-						<a href='/categories/end-to-end-projects'>End-to-End Projects</a>
-					</li>
-					<li>
-						<a href='/categories'>Sitemap</a>
-					</li>
-				</ul>
+				<div className='footer-column'>
+					<ul>
+						<li>
+							<a href='/categories/graphics-design'>Graphics &amp; Design</a>
+						</li>
+						<li>
+							<a href='/categories/online-marketing'>Digital Marketing</a>
+						</li>
+						<li>
+							<a href='/categories/writing-translation'>Writing &amp; Translation</a>
+						</li>
+						<li>
+							<a href='/categories/video-animation'>Video &amp; Animation</a>
+						</li>
+						<li>
+							<a href='/categories/music-audio'>Music &amp; Audio</a>
+						</li>
+						<li>
+							<a href='/logo-maker?source=footer'>Fiverr Logo Maker</a>
+						</li>
+						<li>
+							<a href='/categories/programming-tech'>Programming &amp; Tech</a>
+						</li>
+						<li>
+							<a href='/categories/data'>Data</a>
+						</li>
+						<li>
+							<a href='/categories/business'>Business</a>
+						</li>
+						<li>
+							<a href='/categories/lifestyle'>Personal Growth &amp; Hobbies</a>
+						</li>
+						<li>
+							<a href='/categories/photography'>Photography</a>
+						</li>
+						<li>
+							<a href='/categories/end-to-end-projects'>End-to-End Projects</a>
+						</li>
+						<li>
+							<a href='/categories'>Sitemap</a>
+						</li>
+					</ul>
+				</div>
 				{/* Col 2 */}
-				<ul>
-					<li>
-						<a href='/jobs?source=footer' rel=''>
-							Careers
-						</a>
-					</li>
-					<li>
-						<a href='/news/press-releases?source=footer' rel=''>
-							Press &amp; News
-						</a>
-					</li>
-					<li>
-						<a href='/partnerships?source=footer' rel=''>
-							Partnerships
-						</a>
-					</li>
-					<li>
-						<a href='/legal-portal/privacy/privacy-policy?source=footer' rel=''>
-							Privacy Policy
-						</a>
-					</li>
-					<li>
-						<a href='/legal-portal?source=footer' rel=''>
-							Terms of Service
-						</a>
-					</li>
-					<li>
-						<a href='/legal-portal/community/intellectual-property?source=footer' rel=''>
-							Intellectual Property Claims
-						</a>
-					</li>
-					<li>
-						<a href='https://investors.fiverr.com' target='_blank' rel=''>
-							Investor Relations
-						</a>
-					</li>
-				</ul>
+				<div className='footer-column'>
+					<ul>
+						<li>
+							<a href='/jobs?source=footer' rel=''>
+								Careers
+							</a>
+						</li>
+						<li>
+							<a href='/news/press-releases?source=footer' rel=''>
+								Press &amp; News
+							</a>
+						</li>
+						<li>
+							<a href='/partnerships?source=footer' rel=''>
+								Partnerships
+							</a>
+						</li>
+						<li>
+							<a href='/legal-portal/privacy/privacy-policy?source=footer' rel=''>
+								Privacy Policy
+							</a>
+						</li>
+						<li>
+							<a href='/legal-portal?source=footer' rel=''>
+								Terms of Service
+							</a>
+						</li>
+						<li>
+							<a href='/legal-portal/community/intellectual-property?source=footer' rel=''>
+								Intellectual Property Claims
+							</a>
+						</li>
+						<li>
+							<a href='https://investors.fiverr.com' target='_blank' rel=''>
+								Investor Relations
+							</a>
+						</li>
+					</ul>
+				</div>
 				{/* Col 3 */}
-				<ul>
-					<li>
-						<a href='https://help.fiverr.com/hc/en-us' target='_blank' rel='noopener'>
-							Help &amp; Support
-						</a>
-					</li>
-					<li>
-						<a href='/trust_safety?source=footer' rel=''>
-							Trust &amp; Safety
-						</a>
-					</li>
-					<li>
-						<a href='https://help.fiverr.com/hc/en-us/articles/25616781230481-Fiverr-s-guide-to-quality-and-trust' target='_blank' rel='noopener'>
-							Quality Guide
-						</a>
-					</li>
-					<li>
-						<a href='https://help.fiverr.com/hc/en-us/articles/360010451297-How-to-start-selling-on-Fiverr' target='_blank' rel='noopener'>
-							Selling on Fiverr
-						</a>
-					</li>
-					<li>
-						<a href='/cp/how-fiverr-works?show_join&amp;source=footer' rel=''>
-							Buying on Fiverr
-						</a>
-					</li>
-					<li>
-						<a href='/resources/guides?source=footer' target='_blank' rel='noopener'>
-							Fiverr Guides
-						</a>
-					</li>
-					<li>
-						<a href='https://learn.fiverr.com' target='_blank' rel='noopener'>
-							Learn<p>Online Courses</p>
-						</a>
-					</li>
-				</ul>
+				<div className='footer-column'>
+					<ul>
+						<li>
+							<a href='https://help.fiverr.com/hc/en-us' target='_blank' rel='noopener'>
+								Help &amp; Support
+							</a>
+						</li>
+						<li>
+							<a href='/trust_safety?source=footer' rel=''>
+								Trust &amp; Safety
+							</a>
+						</li>
+						<li>
+							<a href='https://help.fiverr.com/hc/en-us/articles/25616781230481-Fiverr-s-guide-to-quality-and-trust' target='_blank' rel='noopener'>
+								Quality Guide
+							</a>
+						</li>
+						<li>
+							<a href='https://help.fiverr.com/hc/en-us/articles/360010451297-How-to-start-selling-on-Fiverr' target='_blank' rel='noopener'>
+								Selling on Fiverr
+							</a>
+						</li>
+						<li>
+							<a href='/cp/how-fiverr-works?show_join&amp;source=footer' rel=''>
+								Buying on Fiverr
+							</a>
+						</li>
+						<li>
+							<a href='/resources/guides?source=footer' target='_blank' rel='noopener'>
+								Fiverr Guides
+							</a>
+						</li>
+						<li>
+							<a href='https://learn.fiverr.com' target='_blank' rel='noopener'>
+								Learn<p>Online Courses</p>
+							</a>
+						</li>
+					</ul>
+				</div>
 				{/* Col 4 */}
-				<ul>
-					<li>
-						<a href='/cp/success-stories?source=footer' target='_blank' rel=''>
-							Customer Success Stories
-						</a>
-					</li>
-					<li>
-						<a href='https://community.fiverr.com' target='_blank' rel='noopener'>
-							Community Hub
-						</a>
-					</li>
-					<li>
-						<a href='https://community.fiverr.com/forums/' target='_blank' rel='noopener'>
-							Forum
-						</a>
-					</li>
-					<li>
-						<a href='https://events.fiverr.com' target='_blank' rel='noopener nofollow'>
-							Events
-						</a>
-					</li>
-					<li>
-						<a href='https://blog.fiverr.com/?utm_source=fiverr&amp;utm_medium=website' target='_blank' rel='noopener'>
-							Blog
-						</a>
-					</li>
-					<li>
-						<a href='/partnerships/creators' target='_blank' rel='noopener'>
-							Creators
-						</a>
-					</li>
-					<li>
-						<a href='/partnerships/affiliates' target='_blank' rel='noopener'>
-							Affiliates
-						</a>
-					</li>
-					<li>
-						<a href='https://play.acast.com/s/ninetwentynine' target='_blank' rel='noopener nofollow'>
-							Podcast
-						</a>
-					</li>
-					<li>
-						<a href='/referral_program?source=footer' rel=''>
-							Invite a Friend
-						</a>
-					</li>
-					<li>
-						<a href='/start_selling?source=footer' rel=''>
-							Become a Seller
-						</a>
-					</li>
-					<li>
-						<a href='/community/standards?source=footer' target='_blank' rel=''>
-							Community Standards
-						</a>
-					</li>
-				</ul>
+				<div className='footer-column'>
+					<ul>
+						<li>
+							<a href='/cp/success-stories?source=footer' target='_blank' rel=''>
+								Customer Success Stories
+							</a>
+						</li>
+						<li>
+							<a href='https://community.fiverr.com' target='_blank' rel='noopener'>
+								Community Hub
+							</a>
+						</li>
+						<li>
+							<a href='https://community.fiverr.com/forums/' target='_blank' rel='noopener'>
+								Forum
+							</a>
+						</li>
+						<li>
+							<a href='https://events.fiverr.com' target='_blank' rel='noopener nofollow'>
+								Events
+							</a>
+						</li>
+						<li>
+							<a href='https://blog.fiverr.com/?utm_source=fiverr&amp;utm_medium=website' target='_blank' rel='noopener'>
+								Blog
+							</a>
+						</li>
+						<li>
+							<a href='/partnerships/creators' target='_blank' rel='noopener'>
+								Creators
+							</a>
+						</li>
+						<li>
+							<a href='/partnerships/affiliates' target='_blank' rel='noopener'>
+								Affiliates
+							</a>
+						</li>
+						<li>
+							<a href='https://play.acast.com/s/ninetwentynine' target='_blank' rel='noopener nofollow'>
+								Podcast
+							</a>
+						</li>
+						<li>
+							<a href='/referral_program?source=footer' rel=''>
+								Invite a Friend
+							</a>
+						</li>
+						<li>
+							<a href='/start_selling?source=footer' rel=''>
+								Become a Seller
+							</a>
+						</li>
+						<li>
+							<a href='/community/standards?source=footer' target='_blank' rel=''>
+								Community Standards
+							</a>
+						</li>
+					</ul>
+				</div>
 				{/* Col 5 */}
-				<ul>
-					<li>
-						<a href='/fiverr-business-solutions?source=footer' target='_blank' rel='noopener'>
-							About Business Solutions
-						</a>
-					</li>
-					<li>
-						<a href='https://pro.fiverr.com?source=footer' target='_blank' rel='noopener'>
-							Fiverr Pro
-						</a>
-					</li>
-					<li>
-						<a href='/certified?source=footer' target='_blank' rel='noopener'>
-							Fiverr Certified
-						</a>
-					</li>
-					<li>
-						<a href='/cp/agencies-waiting-list?source=footer' target='_blank' rel='noopener'>
-							Become an Agency
-						</a>
-					</li>
-					<li>
-						<a href='https://enterprise.fiverr.com/?source=footer' target='_blank' rel='noopener'>
-							Fiverr Enterprise
-						</a>
-					</li>
-					<li>
-						<a href='https://www.clearvoice.com?source=footer' target='_blank' rel='noopener'>
-							ClearVoice<p>Content Marketing</p>
-						</a>
-					</li>
-					<li>
-						<a href='https://workingnotworking.com?source=footer' target='_blank' rel='noopener'>
-							Working Not Working
-						</a>
-					</li>
-					<li>
-						<a href='/fbs-contact-us?source=footer' target='_blank' rel='noopener'>
-							Contact Sales
-						</a>
-					</li>
-				</ul>
+				<div className='footer-column'>
+					<ul>
+						<li>
+							<a href='/fiverr-business-solutions?source=footer' target='_blank' rel='noopener'>
+								About Business Solutions
+							</a>
+						</li>
+						<li>
+							<a href='https://pro.fiverr.com?source=footer' target='_blank' rel='noopener'>
+								Fiverr Pro
+							</a>
+						</li>
+						<li>
+							<a href='/certified?source=footer' target='_blank' rel='noopener'>
+								Fiverr Certified
+							</a>
+						</li>
+						<li>
+							<a href='/cp/agencies-waiting-list?source=footer' target='_blank' rel='noopener'>
+								Become an Agency
+							</a>
+						</li>
+						<li>
+							<a href='https://enterprise.fiverr.com/?source=footer' target='_blank' rel='noopener'>
+								Fiverr Enterprise
+							</a>
+						</li>
+						<li>
+							<a href='https://www.clearvoice.com?source=footer' target='_blank' rel='noopener'>
+								ClearVoice<p>Content Marketing</p>
+							</a>
+						</li>
+						<li>
+							<a href='https://workingnotworking.com?source=footer' target='_blank' rel='noopener'>
+								Working Not Working
+							</a>
+						</li>
+						<li>
+							<a href='/fbs-contact-us?source=footer' target='_blank' rel='noopener'>
+								Contact Sales
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 			{/* Logo */}
 			<div className='footer-title flex align-center justify-center'>
