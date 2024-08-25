d69c3baaea39b706b67fc23380e391aac32d88bc
ForkliftOctober <ork16oct@gmail.com>
2024-08-25 19:01:52 +0300

    Add functions

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { gigService } from '../services/gig/gig.service.local.js'
 
 import StarIcon from '../assets/svg/StarIcon.svg?react'
 
@@ -9,8 +10,27 @@ import StarIcon from '../assets/svg/StarIcon.svg?react'
 
 // TODO: handle external data
 // TODO: dynamic star icons
+// TODO: move file to preview page
 
 export function GigDetails() {
+	const [gig, setGig] = useState(null)
+	const { gigId } = useParams()
+
+	useEffect(() => {
+		if (gigId) loadGig()
+	}, [gigId])
+
+	function loadGig() {
+		gigService
+			.getById(gigId)
+			.then(gig => setGig(gig))
+			.catch(err => {
+				console.log('Had issues in gig details', err)
+				navigate('/gig')
+			})
+	}
+	if (!gig) return <div>Loading...</div>
+
 	return (
 		<div id='root'>
 			<div className='main-layout base-layout'>
