3b544876b4897caed14862212d5dc96385c63e14
ForkliftOctober <ork16oct@gmail.com>
2024-08-26 11:46:17 +0300

    Change file name

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -5,7 +5,7 @@ import { GigDetails } from './pages/GigDetails.jsx'
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { GigPage } from './pages/GigsPage.jsx'
-import { GigPreview } from './pages/GigPreviewPage.jsx'
+import { GigDetails } from './pages/GigDetailsPage.jsx'
 
 export function RootCmp() {
 	return (
@@ -15,7 +15,7 @@ export function RootCmp() {
 				<Routes>
 					<Route path='/' element={<HomePage />} />
 					<Route path='/category/:gigs' element={<GigPage />} />
-					<Route path='/username/:gigId' element={<GigPreview />} />
+					<Route path='/username/:gigId' element={<GigDetails />} />
 				</Routes>
 			</main>
 			<AppFooter />
--- src/pages/GigDetailsPage.jsx
+++ src/pages/GigDetailsPage.jsx
@@ -2,13 +2,15 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { gigService } from '../services/gig/gig.service.local.js'
 import { GigDetails } from '../cmps/GigDetails'
 
 export function GigPreview() {
 	return (
 		<section>
 			<GigDetails />
+			<GigCarrousel />
+			<GigAbout />
+
 			{/* <GigPricing /> */}
 			{/* <GigReviews /> */}
 		</section>
