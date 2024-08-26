66ee4ec6f41d87cc96b7171bdb7454e92028aec7
ForkliftOctober <ork16oct@gmail.com>
2024-08-26 08:04:30 +0300

    Change details files

--- src/pages/GigPreviewPage.jsx
+++ src/pages/GigPreviewPage.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { gigService } from '../services/gig/gig.service.local.js'
+import { GigDetails } from '../cmps/GigDetails'
 
 export function GigPreview() {
-    return <h1>yooooo</h1>
+	return (
+		<section>
+			<GigDetails />
+			{/* <GigPricing /> */}
+			{/* <GigReviews /> */}
+		</section>
+	)
 }
