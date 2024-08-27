a4ad338df64e471065c2a7408272f37b36ee0ff5
ForkliftOctober <ork16oct@gmail.com>
2024-08-27 12:27:41 +0300

    Add gig temp owner

--- src/cmps/GigDetails.jsx
+++ src/cmps/GigDetails.jsx
@@ -1,29 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { gigService } from '../services/gig/gig.service.local.js'
 
-import StarIcon from '../assets/svg/StarIcon.svg?react'
-
 export function GigDetails() {
-	// const [gig, setGig] = useState(null)
-	// const { gigId } = useParams()
+	const [gig, setGig] = useState(null)
+	const { gigId } = useParams()
 
-	// useEffect(() => {
-	// 	if (gigId) loadGig()
-	// }, [gigId])
+	useEffect(() => {
+		if (gigId) loadGig()
+	}, [gigId])
 
-	// function loadGig() {
-	// 	gigService
-	// 		.getById(gigId)
-	// 		.then(gig => setGig(gig))
-	// 		.catch(err => {
-	// 			console.log('Had issues in gig details', err)
-	// 			navigate('/gig')
-	// 		})
-	// }
-	// if (!gig) return <div>Loading...</div>
+	function loadGig() {
+		gigService
+			.getById(gigId)
+			.then(gig => setGig(gig))
+			.catch(err => {
+				console.log('Had issues in gig details', err)
+				navigate('/gig')
+			})
+	}
+	if (!gig) return <div>Loading...</div>
 
-	return <h1>details</h1>
+	return (
+		<section className='gig-overview'>
+			<h1 className='text-display-3'>{gig.title}</h1>
+			<div tabIndex='0' role='button' aria-label='Scroll to about the seller' className='o6KMeAI tbody-4  text-bold'>
+				<p>{gig.owner}</p>
+				<p>Price: ${gig.price}</p>
+				<p>Days to make: {gig.daysToMake}</p>
+			</div>
+		</section>
+	)
 }
