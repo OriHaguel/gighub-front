291dd33d713ee792a9cbafc94a43a40b2dc5946e
ForkliftOctober <ork16oct@gmail.com>
2024-08-29 10:17:39 +0300

    Fix header alignment

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,11 +2,13 @@ import { Link, NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { setFilterBy } from '../store/actions/gig.actions'
 import React, { useEffect, useState } from 'react'
+
+// Image Imports
 import HeaderLogo from '../assets/svg/Gighub_logo.svg?react'
 import HeaderNavicon from '../assets/svg/HeaderNavicon.svg?react'
 import ChevronIcon from '../assets/svg/ChevronIcon.svg?react'
-import { setFilterBy } from '../store/actions/gig.actions'
 import Globe from '../assets/svg/Globe.svg?react'
 import ModalLoginSignupPic from '../assets/img/modal-login-signup.png'
 
@@ -14,6 +16,7 @@ export function AppHeader() {
 	const navigate = useNavigate()
 	const [inputValue, setInputValue] = useState({ txt: '' })
 	const filterBy = useSelector(state => state.gigModule.filterBy)
+
 	const [isVisible, setIsVisible] = useState(false)
 	const [isModalOpen, setIsModalOpen] = useState(false)
 
@@ -67,7 +70,6 @@ export function AppHeader() {
 		setIsModalOpen(true)
 	}
 
-
 	const closeModal = () => {
 		setIsModalOpen(false)
 	}
@@ -84,34 +86,36 @@ export function AppHeader() {
 							<HeaderLogo />
 						</Link>
 						<div className={`fiverr-header-search-animated ${isVisible ? 'visible' : ''}`}>
-							<form className="search-form dark" onSubmit={onSubmit}>
-								<input type="search" placeholder="What service are you looking for today?" onChange={handleChange} value={inputValue.txt} name='txt' />
-								<button className="submit-button dark-search-button" type='submit'>
-									<div className="submit-button-icon">
-										<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="m15.89 14.653-3.793-3.794a.37.37 0 0 0-.266-.109h-.412A6.499 6.499 0 0 0 6.5 0C2.91 0 0 2.91 0 6.5a6.499 6.499 0 0 0 10.75 4.919v.412c0 .1.04.194.11.266l3.793 3.794a.375.375 0 0 0 .531 0l.707-.707a.375.375 0 0 0 0-.53ZM6.5 11.5c-2.763 0-5-2.238-5-5 0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5 0 2.762-2.238 5-5 5Z"></path></svg>
+							<form className='search-form dark' onSubmit={onSubmit}>
+								<input type='search' placeholder='What service are you looking for today?' onChange={handleChange} value={inputValue.txt} name='txt' />
+								<button className='submit-button dark-search-button' type='submit'>
+									<div className='submit-button-icon'>
+										<svg width='16' height='16' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg' fill='white'>
+											<path d='m15.89 14.653-3.793-3.794a.37.37 0 0 0-.266-.109h-.412A6.499 6.499 0 0 0 6.5 0C2.91 0 0 2.91 0 6.5a6.499 6.499 0 0 0 10.75 4.919v.412c0 .1.04.194.11.266l3.793 3.794a.375.375 0 0 0 .531 0l.707-.707a.375.375 0 0 0 0-.53ZM6.5 11.5c-2.763 0-5-2.238-5-5 0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5 0 2.762-2.238 5-5 5Z'></path>
+										</svg>
 									</div>
 								</button>
 							</form>
 						</div>
-						<nav className="fiverr-nav">
-							<ul>
+						<nav className='fiverr-nav'>
+							<ul className='header-navigation-container'>
 								<li>
-									<NavLink to='/fiverr-pro' className='pro'>
+									<NavLink to='/fiverr-pro' className='header-pro'>
 										Fiverr Pro <ChevronIcon />
 									</NavLink>
 								</li>
 								<li>
-									<NavLink to='/explore' className='explore'>
+									<NavLink to='/explore' className='header-explore'>
 										Explore <ChevronIcon />
 									</NavLink>
 								</li>
 								<li>
-									<NavLink to='/language' className='language'>
+									<NavLink to='/language' className='header-language'>
 										<Globe /> English
 									</NavLink>
 								</li>
 								<li>
-									<NavLink to='/become-seller' className='seller'>
+									<NavLink to='/become-seller' className='header-seller'>
 										Become a Seller
 									</NavLink>
 								</li>
@@ -139,7 +143,9 @@ export function AppHeader() {
 			{/* <button className="login-signup-modal" onClick={openModal}>open modal</button> */}
 
 			<dialog className='modal' id='modal' open={isModalOpen}>
-				<button className='button close-button' onClick={closeModal}>X</button>
+				<button className='button close-button' onClick={closeModal}>
+					X
+				</button>
 				<img src={ModalLoginSignupPic} alt='modal' className='modal-image' />
 				<div className='modal-txt'>
 					<h2>Success starts here</h2>
@@ -158,11 +164,11 @@ export function AppHeader() {
 						Password
 						<input type='pass' />
 					</label>
-					<button className='button' type='submit'>Continue</button>
+					<button className='button' type='submit'>
+						Continue
+					</button>
 				</form>
 			</dialog>
-
-
 		</div>
 	)
 }
