24ebce93b5895db971ed5d0ed0b32cbd83fbe8e8
ForkliftOctober <ork16oct@gmail.com>
2024-08-31 20:43:28 +0300

    Set basic about

--- src/cmps/GigAbout.jsx
+++ src/cmps/GigAbout.jsx
@@ -1,14 +1,39 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { gigService } from '../services/gig/gig.service.local.js'
 
 export function GigAbout() {
+	const param = useParams()
+	const [gig, setGig] = useState(null)
+
+	useEffect(() => {
+		loadGig()
+	}, [param.gigId])
+
+	async function loadGig() {
+		try {
+			const gig = await gigService.getById(param.gigId)
+			setGig(gig)
+		} catch (error) {
+			console.log('🚀 ~ loadGig ~ error:', error)
+			// Assuming navigate is a function to redirect to another page
+			// navigate('/gigs')
+		}
+	}
+
+	if (!gig)
+		return (
+			<section className='about-details'>
+				<h1 className='about-gig'>Loading...</h1>
+			</section>
+		)
+
 	return (
 		<section className='about-details'>
 			<h1 className='about-gig'>About this gig</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel velit id ex posuere placerat. Nullam tristique, velit non consectetur sagittis, enim felis bibendum neque, at euismod est velit vel neque. Nullam convallis, ex et interdum facilisis, metus nunc ullamcorper tellus, vel semper odio neque vel velit. Sed non enim sed metus ultricies tincidunt. Nulla facilisi. Donec semper velit sed est efficitur, non consectetur nunc varius. Nullam malesuada, ipsum eu consectetur pulvinar, velit neque consectetur lectus, vel eleifend velit felis eu felis. Maecenas non ipsum vel mi consectetur condimentum. Sed auctor, metus non ultricies tristique, sapien ipsum pharetra sapien, ut rutrum neque nisi sed mi. Donec non purus vel ipsum consectetur scelerisque. Donec volutpat, arcu sed facilisis al</p>
+			<p>{gig.aboutGig}</p>
 		</section>
 	)
 }
