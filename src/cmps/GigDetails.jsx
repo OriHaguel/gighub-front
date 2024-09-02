f16ffd40422e315171a387fff61395a06b44efd1
ForkliftOctober <ork16oct@gmail.com>
2024-09-02 15:12:46 +0300

    Fix seller pro icon

--- src/cmps/GigDetails.jsx
+++ src/cmps/GigDetails.jsx
@@ -4,11 +4,6 @@ import { gigService } from '../services/gig/gig.service.local.js'
 import { SellerLevel } from './SellerLevel.jsx'
 import { SellerPro } from './SellerPro.jsx'
 import Star from '../assets/svg/star.svg?react'
-import { Carousel } from "react-responsive-carousel"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import GigImage from '../assets/img/gig-image.png'
-import CarrouselControlNext from '../assets/svg/CarrouselControlNext.svg?react'
-import CarrouselControlPrev from '../assets/svg/CarrouselControlPrev.svg?react'
 
 export function GigDetails() {
 	const [gig, setGig] = useState(null)
@@ -17,7 +12,6 @@ export function GigDetails() {
 
 	useEffect(() => {
 		if (gigId) loadGig()
-
 	}, [gigId])
 
 	function loadGig() {
@@ -30,53 +24,19 @@ export function GigDetails() {
 			})
 	}
 
-	const renderArrowPrev = (clickHandler, hasPrev) => (
-		hasPrev && (
-			<button
-				type="button"
-				className="detail-prev-button"
-				onClick={(e) => {
-					e.stopPropagation()
-					console.log("Prev clicked")
-					clickHandler()
-				}}
-			>
-				<CarrouselControlPrev className='main-carousel-chevron' />
-			</button>
-		)
-	)
-
-	const renderArrowNext = (clickHandler, hasNext) => (
-		hasNext && (
-			<button
-				type="button"
-				className="detail-next-button"
-				onClick={(e) => {
-					e.stopPropagation()
-					console.log("Next clicked")
-					clickHandler()
-				}}
-			>
-				<CarrouselControlNext className='main-carousel-chevron' />
-			</button>
-		)
-	)
-
 	if (!gig) return <div>Loading...</div>
-	console.log("🚀 ~ GigDetails ~ gig:", gig.img)
+	console.log('🚀 ~ GigDetails ~ gig:', gig.img)
+
+	// Carrousel?
 	if (gig.img === undefined) {
 		gig.img = [GigImage, GigImage, GigImage]
 	}
+
 	return (
 		<section className='gig-overview'>
-
 			<h1 className='gig-title'>{gig.title}</h1>
 			<div className='owner-details'>
-				<img
-					className='owner-image'
-					src={gig.ownerImage || 'fallback-image.png'}
-					alt={gig.owner || 'Owner'}
-				/>
+				<img className='owner-image' src={gig.ownerImage || 'fallback-image.png'} alt={gig.owner || 'Owner'} />
 				<div className='owner-details-container'>
 					<div className='first-line'>
 						<p className='owner-name'>{gig.owner}</p>
@@ -91,34 +51,6 @@ export function GigDetails() {
 							<p>{gig.ownerRating}</p>
 							<p className='light-text'>{gig.ownerOrders} orders in queue</p>
 						</div>
-					</div>
-					<div className='gig-detail-carousel'>
-						<Carousel
-							showArrows={true}
-							autoPlay={false}
-							infiniteLoop={true}
-							showThumbs={true}
-							dynamicHeight={false}
-							emulateTouch={false}
-							showIndicators={false}
-							showStatus={false}
-							renderArrowPrev={renderArrowPrev}
-							renderArrowNext={renderArrowNext}
-
-						>
-
-							{gig.img && gig.img.map((mediaUrl, index) => (
-								mediaUrl.endsWith('.mp4') ? (
-									<div className="gig-list-vid-container" key={index}>
-										<video src={mediaUrl} alt={`Gig Video ${index + 1}`} controls />
-									</div>
-								) : (
-									<div className="gig-list-img-container" key={index}>
-										<img src={mediaUrl} alt={`Gig Image ${index + 1}`} />
-									</div>
-								)
-							))}
-						</Carousel>
 					</div>
 				</div>
 			</div>
