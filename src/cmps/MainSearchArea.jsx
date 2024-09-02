75854fb961cdd7d566b84f0634b30ef0d2ba6a0c
ForkliftOctober <ork16oct@gmail.com>
2024-09-03 01:52:50 +0300

    Fix hero section

--- src/cmps/MainSearchArea.jsx
+++ src/cmps/MainSearchArea.jsx
@@ -1,38 +1,51 @@
-
-import hero from '../assets/img/hero-section.jpeg'
-import SearchLogo from '../assets/svg/searchLogo.svg?react'
 import { SearchInput } from './SearchInput'
+
+// Image imports
+import hero from '../assets/img/hero-section.jpeg'
 import MetaLogo from '../assets/svg/MetaLogo.svg?react'
 import GoogleLogo from '../assets/svg/GoogleLogo.svg?react'
 import NetflixLogo from '../assets/svg/NetflixLogo.svg?react'
 import PAndGLogo from '../assets/svg/pAndGLogo.svg?react'
 import PayPalLogo from '../assets/svg/PayPalLogo.svg?react'
 import PayoneerLogo from '../assets/svg/PayoneerLogo.svg?react'
+// import SearchLogo from '../assets/svg/searchLogo.svg?react'
 
 export function MainSearchArea() {
+	return (
+		<div className='hero-container'>
+			<img src={hero} alt='Green-Hero' className='hero-image' />
+			<div className='hero-content'>
+				<div className='main-hero'>
+					<h1 className='promotion-txt'>
+						Scale your professional workforce with <i className='freelancers-txt'>freelancers</i>
+					</h1>
 
-    return (
-        <div className="hero-container">
-            <img src={hero} alt="Green-Hero" className="hero-image" />
-            <div className="hero-content">
-                <div className='promotion-txt'>
-                    <h1 className='txt-content'>Scale your professional workforce with <i className='freelancers-txt'>freelancers</i></h1>
-                </div>
-
-                <SearchInput />
-                <div className="logo-section">
-                    <ul className="logo-list">
-                        <div>Trusted By:</div>
-                        <li><MetaLogo /></li>
-                        <li><GoogleLogo /></li>
-                        <li><NetflixLogo /></li>
-                        <li><PAndGLogo /></li>
-                        <li><PayPalLogo /></li>
-                        <li><PayoneerLogo /></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    )
+					<SearchInput />
+				</div>
+				<div className='logo-section'>
+					<ul className='logo-list'>
+						<p>Trusted By:</p>
+						<li>
+							<MetaLogo />
+						</li>
+						<li>
+							<GoogleLogo />
+						</li>
+						<li>
+							<NetflixLogo />
+						</li>
+						<li>
+							<PAndGLogo />
+						</li>
+						<li>
+							<PayPalLogo />
+						</li>
+						<li>
+							<PayoneerLogo />
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
-
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,39 +10,33 @@ import { setFilterBy } from '../store/actions/gig.actions.js'
 import { gigService } from '../services/gig/index.js'
 
 export function HomePage() {
+	useEffect(() => {
+		setFilterBy(gigService.getDefaultFilter())
+	}, [])
 
-
-    useEffect(() => {
-
-        setFilterBy(gigService.getDefaultFilter())
-
-    }, [])
-
-    return (
-
-        <div className="main-page">
-
-            {/* <section className="main-header">
+	return (
+		<div className='main-page'>
+			{/* <section className="main-header">
                 <AppHeader />
             </section> */}
 
-            <section className="main-search-area">
-                <MainSearchArea />
-            </section>
+			<section className='main-search-area'>
+				<MainSearchArea />
+			</section>
 
-            <section className="category-list">
-                <CategoryList />
-            </section>
+			<section className='category-list'>
+				<CategoryList />
+			</section>
 
-            <section className="home-page-carrousel">
-                <h1>Popular Services</h1>
-                <PopularServicesCarrousel />
-            </section>
+			<section className='home-page-carrousel'>
+				<h1>Popular services</h1>
+				<PopularServicesCarrousel />
+			</section>
 
-            <section className='advert-area'>
-                <h1>Advert Area</h1>
-                <AdvertArea />
-            </section>
-        </div>
-    )
+			<section className='advert-area'>
+				<h1>Advert Area</h1>
+				<AdvertArea />
+			</section>
+		</div>
+	)
 }
