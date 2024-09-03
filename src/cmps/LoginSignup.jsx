dc59ad751310a81165acd7d1f66bb2d8f3bd43cf
ForkliftOctober <ork16oct@gmail.com>
2024-09-03 14:30:40 +0300

    Add modal styling

--- src/cmps/LoginSignup.jsx
+++ src/cmps/LoginSignup.jsx
@@ -1,117 +1,122 @@
 import { useState } from 'react'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
 
-
 // import { login, signup } from '../store/actions/user.action.js'
 import { userService } from '../services/user/index.js'
 import { login, signup } from '../store/actions/user.actions.js'
 // import { signup, login } from '../store/user.action.js'
 
-
 // const { useState } = React
 
 export function LoginSignup({ elModal, onClose }) {
+	const [isSignup, setIsSignUp] = useState(false)
+	const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
 
-    const [isSignup, setIsSignUp] = useState(false)
-    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+	function handleChange({ target }) {
+		const { name: field, value } = target
+		setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
+	}
 
-    function handleChange({ target }) {
-        const { name: field, value } = target
-        setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
-    }
+	function handleSubmit(ev) {
+		ev.preventDefault()
+		onLogin(credentials)
+	}
 
-    function handleSubmit(ev) {
-        ev.preventDefault()
-        onLogin(credentials)
-    }
+	function onLogin(credentials) {
+		const method = isSignup ? signup : login
 
+		method(credentials)
+		// .then(user => showSuccessMsg('Hello!'))
+		// .catch(err => showErrorMsg('Error logging in'))
+	}
 
-    function onLogin(credentials) {
-        const method = isSignup ? signup : login
+	function onCloseModal() {
+		elModal.current.close()
+		onClose()
+	}
+	return (
+		// <div className="login-page">
+		//     <form className="login-form" onSubmit={handleSubmit}>
+		//         <input
+		//             type="text"
+		//             name="username"
+		//             value={credentials.username}
+		//             placeholder="Username"
+		//             onChange={handleChange}
+		//             required
+		//             autoFocus
+		//         />
+		//         <input
+		//             type="password"
+		//             name="password"
+		//             value={credentials.password}
+		//             placeholder="Password"
+		//             onChange={handleChange}
+		//             required
+		//             autoComplete="off"
+		//         />
+		//         {isSignup && <input
+		//             type="text"
+		//             name="fullname"
+		//             value={credentials.fullname}
+		//             placeholder="Full name"
+		//             onChange={handleChange}
+		//             required
+		//         />}
+		//         <button>{isSignup ? 'Signup' : 'Login'}</button>
+		//     </form>
 
-        method(credentials)
-        // .then(user => showSuccessMsg('Hello!'))
-        // .catch(err => showErrorMsg('Error logging in'))
-    }
-
-
-    function onCloseModal() {
-        elModal.current.close()
-        onClose()
-    }
-    return (
-        // <div className="login-page">
-        //     <form className="login-form" onSubmit={handleSubmit}>
-        //         <input
-        //             type="text"
-        //             name="username"
-        //             value={credentials.username}
-        //             placeholder="Username"
-        //             onChange={handleChange}
-        //             required
-        //             autoFocus
-        //         />
-        //         <input
-        //             type="password"
-        //             name="password"
-        //             value={credentials.password}
-        //             placeholder="Password"
-        //             onChange={handleChange}
-        //             required
-        //             autoComplete="off"
-        //         />
-        //         {isSignup && <input
-        //             type="text"
-        //             name="fullname"
-        //             value={credentials.fullname}
-        //             placeholder="Full name"
-        //             onChange={handleChange}
-        //             required
-        //         />}
-        //         <button>{isSignup ? 'Signup' : 'Login'}</button>
-        //     </form>
-
-        //     <div className="btns">
-        //         <a href="#" onClick={() => setIsSignUp(!isSignup)}>
-        //             {isSignup ?
-        //                 'Already a member? Login' :
-        //                 'New user? Signup here'
-        //             }
-        //         </a >
-        //     </div>
-        // </div >
-        <div>
-
-            <form className='form' method='dialog' onSubmit={handleSubmit}>
-                <h2>Create a new account</h2>
-                <p>Already have an account? Sign in</p>
-                <label>
-                    Email or username
-                    <input type='text' onChange={handleChange} name='username' />
-                </label>
-                <label>
-                    Password
-                    <input type='password' onChange={handleChange} name='password' />
-                </label>
-                {isSignup &&
-                    <label> Fullname
-                        <input
-                            type="text"
-                            name="fullname"
-                            value={credentials.fullname}
-                            // placeholder="Full name"
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>}
-                {elModal.current && <button className='button' type='submit' onClick={onCloseModal}>Continue</button>}
-                <a href="#" onClick={() => setIsSignUp(!isSignup)}>
-                    {isSignup ?
-                        'Already a member? Login' :
-                        'New user? Signup here'
-                    }
-                </a >
-            </form>
-        </div>
-    )
+		//     <div className="btns">
+		//         <a href="#" onClick={() => setIsSignUp(!isSignup)}>
+		//             {isSignup ?
+		//                 'Already a member? Login' :
+		//                 'New user? Signup here'
+		//             }
+		//         </a >
+		//     </div>
+		// </div >
+		<div className='login-container'>
+			<form className='form' method='dialog' onSubmit={handleSubmit}>
+				<h2>Create a new account</h2>
+				<p className='sign-in'>
+					Already have an account? <span className='link'>Sign in</span>
+				</p>
+				<label className='input-container'>
+					<p className='input-title'>Email or username</p>
+					<input type='text' onChange={handleChange} name='username' value='name@email.com' />
+				</label>
+				<label className='input-container'>
+					<p className='input-title'>Password</p>
+					<input type='password' onChange={handleChange} name='password' className='input-pass' />
+				</label>
+				<div className='model-end'>
+					{isSignup && (
+						<label>
+							{' '}
+							Fullname
+							<input
+								type='text'
+								name='fullname'
+								value={credentials.fullname}
+								// placeholder="Full name"
+								onChange={handleChange}
+								required
+							/>
+						</label>
+					)}
+					{elModal.current && (
+						<button className='button' type='submit' onClick={onCloseModal}>
+							Continue
+						</button>
+					)}
+					<a href='#' onClick={() => setIsSignUp(!isSignup)}>
+						{isSignup ? 'Already a member? Login' : 'New user? Signup here'}
+					</a>
+					<p className='small-txt'>
+						By joining, you agree to the Gighub <span className='link'>Terms of Service</span> and to occasionally receive emails from us. Please read our <span className='link'>Privacy Policy</span> to learn how we use your personal data.
+					</p>
+				</div>
+			</form>
+		</div>
+	)
 }
--- src/cmps/Modal.jsx
+++ src/cmps/Modal.jsx
@@ -1,48 +1,60 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 import { userService } from '../services/user/user.service.local'
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
+import { LoginSignup } from './LoginSignup'
+
+// Image imports
 import ModalLoginSignupPic from '../assets/img/modal-login-signup.png'
-import { LoginSignup } from './LoginSignup';
+import TickIcon from '../assets/svg/TickIcon.svg?react'
 
 export function Modal({ isOpen, onClose }) {
-    const user = useSelector(state => state.userModule.user)
-    const elModal = useRef(null)
-    useEffect(() => {
-        const handleEscKey = (event) => {
-            if (event.key === 'Escape') {
-                onClose();
-            }
-        };
+	const user = useSelector(state => state.userModule.user)
+	const elModal = useRef(null)
+	useEffect(() => {
+		const handleEscKey = event => {
+			if (event.key === 'Escape') {
+				onClose()
+			}
+		}
 
-        if (isOpen) {
-            elModal.current.showModal();
-            document.addEventListener('keydown', handleEscKey);
-            document.body.style.overflow = 'hidden';
-        } else {
-            elModal.current.close();
-            document.removeEventListener('keydown', handleEscKey);
-            document.body.style.overflow = 'auto';
-        }
+		if (isOpen) {
+			elModal.current.showModal()
+			document.addEventListener('keydown', handleEscKey)
+			document.body.style.overflow = 'hidden'
+		} else {
+			elModal.current.close()
+			document.removeEventListener('keydown', handleEscKey)
+			document.body.style.overflow = 'auto'
+		}
 
-        return () => document.removeEventListener('keydown', handleEscKey);
-    }, [isOpen, onClose]);
+		return () => document.removeEventListener('keydown', handleEscKey)
+	}, [isOpen, onClose])
 
-    return (
-
-        <div className={`modal-container ${isOpen && 'relative'}`}>
-            <dialog className='modal' id='modal' ref={elModal} onClick={(e) => {
-                if (e.target === elModal.current) onClose();
-            }}>
-                {/* <button className='button close-button' onClick={closeModal}>X</button> */}
-                <img src={ModalLoginSignupPic} alt='modal' className='modal-image' />
-                <div className='modal-txt'>
-                    <h2>Success starts here</h2>
-                    <p>Over 700 categories</p>
-                    <p>Quality work done faster</p>
-                    <p>Access to talent and businesses across the globe</p>
-                </div>
-                {/* <form className='form' method='dialog'>
+	return (
+		<div className={`modal-container ${isOpen && 'relative'}`}>
+			<dialog
+				className='modal'
+				id='modal'
+				ref={elModal}
+				onClick={e => {
+					if (e.target === elModal.current) onClose()
+				}}>
+				{/* <button className='button close-button' onClick={closeModal}>X</button> */}
+				<img src={ModalLoginSignupPic} alt='modal' className='modal-image' />
+				<div className='modal-txt'>
+					<h2>Success starts here</h2>
+					<p>
+						<TickIcon /> Over 700 categories
+					</p>
+					<p>
+						<TickIcon /> Quality work done faster
+					</p>
+					<p>
+						<TickIcon /> Access to talent and businesses across the globe
+					</p>
+				</div>
+				{/* <form className='form' method='dialog'>
                     <h2>Create a new account</h2>
                     <p>Already have an account? Sign in</p>
                     <label>
@@ -55,8 +67,8 @@ export function Modal({ isOpen, onClose }) {
                     </label>
                     <button className='button' type='submit'>Continue</button>
                 </form> */}
-                <LoginSignup elModal={elModal} onClose={onClose} />
-            </dialog>
-        </div>
-    )
+				<LoginSignup elModal={elModal} onClose={onClose} />
+			</dialog>
+		</div>
+	)
 }
