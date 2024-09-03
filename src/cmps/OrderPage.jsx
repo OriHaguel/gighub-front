5f330f07da1f1e542e65eaeef205ff0630bed7a5
alexl <alexlarik13@gmail.com>
2024-09-03 16:06:25 +0300

    Add bonus info order to order page with icons

--- src/cmps/OrderPage.jsx
+++ src/cmps/OrderPage.jsx
@@ -1,58 +1,105 @@
 import InfoIcon from '../assets/svg/InfoIcon.svg?react'
 import CloseIcon from '../assets/svg/CloseIcon.svg?react'
 import OptionsIcon from '../assets/svg/OptionsIcon.svg?react'
+import PackageIcon from '../assets/svg/PackageIcon.svg?react'
+import ArrowIcon from '../assets/svg/ArrowIcon.svg?react'
+import TimerLogo from '../assets/svg/TimerLogo.svg?react'
+import RecycleLogo from '../assets/svg/RecycleLogo.svg?react'
 export function OrderPage({ gig, onClose }) {
 
     return (
 
-        <div class="order-modal-overlay">
-            <div class="order-modal-content">
-                <button class="close-modal-button" onClick={onClose}>
+        <div className="order-modal-overlay">
+            <div className="order-modal-content">
+                <button className="close-modal-button" onClick={onClose}>
                     <CloseIcon />
                 </button>
-                <div class="order-modal-header">
-                    <h2 class="order-modal-title">Order options</h2>
+                <div className="order-modal-header">
+                    <h2 className="order-modal-title">Order options</h2>
                 </div>
-                <div class="order-modal-body">
-                    <div class="order-option">
-                        <div class="option-header">
+                <div className="order-modal-body">
+                    <div className="order-option">
+                        <div className="option-header">
                             <span>Available Upgrades</span>
-                            <button class="info-button">
+                            <button className="info-button">
                                 <InfoIcon />
                             </button>
                         </div>
-                        <div class="option-list">
-                            <div class="option-item">
-                                <div class="option-info">
-                                    <h4 class="option-title">Include Source File</h4>
-                                    <p class="option-description">Receive the source file in addition to your final work.</p>
+                        <div className="option-list">
+                            <div className="option-item">
+                                <div className="option-info">
+                                    <h4 className="option-title">Include Source File</h4>
+                                    <p className="option-description">Receive the source file in addition to your final work.</p>
                                 </div>
-                                <div class="option-action">
-                                    <p class="option-price">$20</p>
-                                    <button class="add-option-button">
+                                <div className="option-action">
+                                    <p className="option-price">$20</p>
+                                    <button className="add-option-button">
                                         <OptionsIcon />
                                     </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="order-summary">
-                        <h3 class="summary-title">Order Summary</h3>
-                        <ul class="summary-list">
-                            <li class="summary-item">
-                                <span class="summary-item-name">Base Price</span>
-                                <span class="summary-item-price">${gig.price}</span>
+                    <div className="order-summary">
+                        <h3 className="summary-title">Order Summary</h3>
+                        <ul className="summary-list">
+                            <li className="summary-item">
+                                <span className="summary-item-name">Base Price</span>
+                                <span className="summary-item-price">${gig.price}</span>
                             </li>
-                            <li class="summary-item">
-                                <span class="summary-item-name">Total Upgrades</span>
-                                <span class="summary-item-price">$20</span>
+                            <li className="summary-item">
+                                <span className="summary-item-name">Total Upgrades</span>
+                                <span className="summary-item-price">$20</span>
                             </li>
                         </ul>
-                        <div class="total-price">
+                        <div className="total-price">
                             <span>Total</span>
-                            <span class="total-amount">${gig.price + 20}</span>
+                            <span className="total-amount">${gig.price + 20}</span>
                         </div>
-                        <button class="place-order-button">Continue (${gig.price + 20})</button>
+                        <ul className="bonus-info-order-page">
+                            <li>
+                                <article className="package-item">
+                                    <div className="package-header">
+                                        <div className="icon-container">
+                                            <li className="icon-list-item">
+                                                <span className="package-icon" aria-hidden="true">
+                                                    <PackageIcon />
+                                                </span>
+                                                <span className="package-name">Basic package</span>
+                                            </li>
+                                        </div>
+                                        <div className="arrow-container">
+                                            <span className="arrow-icon" aria-hidden="true">
+                                                {/* <ArrowIcon /> */}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    {/* <div >
+                                        <ul className="details-list">
+                                            <li className="detail-item-items-center">
+                                                <span>Speed optimization</span>
+                                            </li>
+                                            <li className="detail-item-items-center-text-normal">
+                                                <span>Browser Caching</span>
+                                            </li>
+                                        </ul>
+                                    </div> */}
+                                </article>
+                            </li>
+                            <li className="flex-items-center-icon-list-item">
+                                <span className="timer-logo" aria-hidden="true" >
+                                    <TimerLogo />
+                                </span>
+                                <span className="detail-info">3-day delivery</span>
+                            </li>
+                            <li className="flex-items-center-icon-list-item">
+                                <span className="recycle-logo" aria-hidden="true" >
+                                    <RecycleLogo />
+                                </span>
+                                <span className="detail-info">No revisions</span>
+                            </li>
+                        </ul>
+                        <button className="place-order-button">Continue (${gig.price + 20})</button>
                         <div>You won’t be charged yet</div>
                     </div>
                 </div>
