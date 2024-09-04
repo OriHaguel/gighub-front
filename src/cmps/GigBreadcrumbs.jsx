6787488da43f9fc24aa085b2df913641aec95300
ForkliftOctober <ork16oct@gmail.com>
2024-09-04 17:45:03 +0300

    Add home function to bread

--- src/cmps/GigBreadcrumbs.jsx
+++ src/cmps/GigBreadcrumbs.jsx
@@ -1,16 +1,23 @@
 // React
 import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 // Images
 import HomeIcon from '../assets/svg/HomeIcon.svg?react'
 
 export function GigBreadcrumbs() {
+	const location = useLocation()
+	const queryParams = new URLSearchParams(location.search)
+
 	return (
 		<div className='breadcrumbs-container'>
-			<HomeIcon />
-			<p>
-				<span>/</span>bread
-			</p>
+			<div className='home-icon'>
+				<a href='/'>
+					<HomeIcon />
+				</a>
+			</div>
+			<span>/</span>
+			<p>{queryParams.get('category') || ''}</p>
 		</div>
 	)
 }
