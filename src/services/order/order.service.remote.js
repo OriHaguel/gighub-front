f1198b8651c741504518781ede9748aa83f60b9a
OriHaguel <orihaguel@gmail.com>
2024-09-06 17:09:49 +0300

    Change remote names

--- src/services/order/order.service.remote.js
+++ src/services/order/order.service.remote.js
@@ -9,28 +9,28 @@ export const orderService = {
 }
 
 async function query(filterBy = { txt: '', price: 0 }) {
-    return httpService.get(`gig`, filterBy)
+    return httpService.get(`order`, filterBy)
 }
 
-function getById(gigId) {
-    return httpService.get(`gig/${gigId}`)
+function getById(orderId) {
+    return httpService.get(`order/${orderId}`)
 }
 
-async function remove(gigId) {
-    return httpService.delete(`gig/${gigId}`)
+async function remove(orderId) {
+    return httpService.delete(`order/${orderId}`)
 }
-async function save(gig) {
-    var savedGig
-    if (gig._id) {
-        savedGig = await httpService.put(`gig/${gig._id}`, gig)
+async function save(order) {
+    var savedOrder
+    if (order._id) {
+        savedOrder = await httpService.put(`order/${order._id}`, order)
     } else {
-        savedGig = await httpService.post('gig', gig)
+        savedOrder = await httpService.post('order', order)
     }
-    return savedGig
+    return savedOrder
 }
 
-async function addGigMsg(gigId, txt) {
-    const savedMsg = await httpService.post(`gig/${gigId}/msg`, { txt })
+async function addOrderMsg(orderId, txt) {
+    const savedMsg = await httpService.post(`order/${orderId}/msg`, { txt })
     return savedMsg
 }
 
