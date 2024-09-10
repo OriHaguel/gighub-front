1a87f0352749187e5efec722ca67c4eefd31bf54
alexl <alexlarik13@gmail.com>
2024-09-10 14:11:06 +0300

    Make login functionality for the navicon header icon modal

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -6,6 +6,8 @@ import { AppFooter } from './cmps/AppFooter'
 import { GigPage } from './pages/GigsPage.jsx'
 import { GigDetailsPage } from './pages/GigDetailsPage.jsx'
 import { Dashboard } from './pages/Dashboard.jsx'
+import { Signup } from './pages/Signup.jsx'
+// import { signup } from './store/actions/user.actions.js'
 
 export function RootCmp() {
 	// useEffect(() => {
@@ -23,6 +25,7 @@ export function RootCmp() {
 					<Route path='/gigs' element={<GigPage />} />
 					<Route path='/username/:gigId' element={<GigDetailsPage />} />
 					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/signup' element={<Signup />} />
 				</Routes>
 			</main>
 			<AppFooter />
